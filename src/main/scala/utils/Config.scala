29014de96a091d5fb87211c777c93066ba9e6dfb
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2025-01-28 15:37:22 +0100

    created new empty project repository

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -5,10 +5,10 @@ object Config {
   // The local directory containing this repository
   val projectDir :String = "/C:/Users/admin/Desktop/Esercizi_Big_Data/lab-cse-24-25"
   // The name of the shared bucket on AWS S3 to read datasets
-  val s3sharedbucketName :String = "unibo-bd2425-egallinucci"
+  // val s3sharedbucketName :String = "unibo-bd2425-egallinucci"
   // The name of my bucket name on AWS S3
-  val s3bucketName :String = "unibo-bd2425-abianchi"
+  //val s3bucketName :String = "unibo-bd2425-abianchi"
   // The path to the credentials file for AWS (if you follow instructions, this should not be updated)
-  val credentialsPath :String = "/aws_credentials.txt"
+  //val credentialsPath :String = "/aws_credentials.txt"
 
 }
